beaa88410a4d3180a58abc7dcd02a253de734e1e
edoardorealini <realini.edoardo@gmail.com>
2020-11-09 22:11:28 +0100

    final for hw1

--- hw_1/src/main.py
+++ hw_1/src/main.py
@@ -1,99 +1,113 @@
-from CompareSets import *
-from Shingling import *
-from MinHashing import *
-from CompareSignatures import *
-
+import argparse
+import itertools
 import time
 
+from CompareSets import CompareSets
+from Shingling import Shingling
+from MinHashing import MinHashing
+from CompareSignatures import CompareSignatures
+from LSH import LSH
+
+
 if __name__ == "__main__":
+    print("Testing and showing the LSH pipeline with parameters")
 
-    doc1 = 3
-    doc2 = 10
+    parser = argparse.ArgumentParser(description='Parameters')
+    parser.add_argument('-ks', dest='k_shingles', type=int, help='Dimension of Shingles',  nargs='?', default=5)
+    parser.add_argument('-nhf', dest='n_hash_functions', type=int, help='Number of hash functions to use for MinHashing',  nargs='?', default=100)
+    parser.add_argument('-b', dest='bands', type=int, help='Bands for LSH procedure',  nargs='?', default=25)
+    parser.add_argument('-st', dest='similarity_threshold', type=float, help='Similarity threshold for LSH procedure',  nargs='?', default=0.1)
+    parser.add_argument('-v', dest='verbose', help='Verbose print of Jaccard and Minhashing',  nargs='?', default=False)
 
-    shingles = 5
-    
-    filename = '../data/' + str(doc1) + '.txt'
-    shingling = Shingling(filename)
-    shingling.load_clean_document()
-    shingling.build_shingles(shingles)
-    shingling.hash_shingles()
-    #print(shingling.shingles)
+    args = parser.parse_args()
 
-    filename2 = '../data/' + str(doc2) + '.txt'
-    shingling2 = Shingling(filename2)
-    shingling2.load_clean_document()
-    shingling2.build_shingles(shingles)
-    shingling2.hash_shingles()
-    #print("start second")
-    #print(shingling2.shingles)
+    print("Shingle dimension: ", args.k_shingles)
+    print("MinHash # functions: ", args.n_hash_functions)
+    print("Number of bands: ", args.bands)
+    print("Similarity threshold: ", args.similarity_threshold)
+    print("Verbose: ", args.verbose)
+    print("\n")
 
-    m = MinHashing()
-    m.compute_signature(100,shingling.hashed_shingles)
-    print(m.signature)
+    # load file and create Shingling objects
+    file_list = []
+    shingling_objects = []
+    for i in range(1, 11):
+        filename = "../data/" + str(i) + ".txt"
+        file_list.append(filename)
+        shingling_objects.append(Shingling(filename))
 
-    print("--------------------------------------")
+    # create shingles and hashed shingles for each document 
+    for shingling in shingling_objects:
+        #print(shingle.document_filename)
+        shingling.load_clean_document()
+        shingling.build_shingles(k_length = args.k_shingles)
+        shingling.hash_shingles()
 
-    m2 = MinHashing()
-    m2.compute_signature(100,shingling2.hashed_shingles)
-    print(m2.signature)   
+    c_set = CompareSets()
+    c_sign = CompareSignatures()
 
-    cs = CompareSets()
-    print("\n\nJaccard Similiarity is: ")
-    print(cs.calculateJaccard(shingling.hashed_shingles, shingling2.hashed_shingles))
-  
-    csign = CompareSignatures()
-    print("Signature Similiarity is: ")
-    print(csign.compare_signatures(m.signature, m2.signature))
-
-    
-    """
-    cs = CompareSets()
-    shs = []
+    # jaccard all possible pairs 
+    pairs = list(itertools.combinations(shingling_objects, 2))
 
     start_time = time.time()
 
-    for i in range(1,13):
-        filename = '../data/' + str(i) + '.txt'
-        shs.append(Shingling(filename))
-        shs[i-1].load_clean_document()
-        start_shingles = time.time()
-        shs[i-1].build_shingles(k_length=5)
-        shs[i-1].hash_shingles()        
-        print("----------------------" + str(i) + "---------------------")
-        print(len(shs[i-1].hashed_shingles))
-        print(len(list(set(shs[i-1].hashed_shingles))))
-        print("Shingling time elapsed: " + str(time.time() - start_shingles))
+    jaccard_list = []
+    for i in range(len(pairs)): 
+        jaccard_list.append(c_set.calculateJaccard(pairs[i][0].hashed_shingles, pairs[i][1].hashed_shingles)) 
+        if (args.verbose): print("Jaccard Similarity between " + str(pairs[i][0].document_id) + " and " + str(pairs[i][1].document_id) + ": " + str(jaccard_list[i]))
 
-    print("Total elapsed time for shingling: " + str(time.time() - start_time) + "\n\n")
+    jaccard_filtered = []
+    for i in range(len(jaccard_list)):
+        if jaccard_list[i] >= args.similarity_threshold:
+            jaccard_filtered.append(tuple((pairs[i][0].document_id, pairs[i][1].document_id)))
 
-    for i in range(0,11):
-    	print("\nJaccard similarity between " + str(i+1) + " " + str(i+2))
-    	print(cs.calculateJaccard(shs[i].hashed_shingles, shs[i+1].hashed_shingles))
-    """
-    
-    # MinHashing
-    #m = MinHashing()
-    #m.compute_signature(100, shs[0].hashed_shingles)
-    #print(m.signature)
+    print("Filtered couples over Threshold - Jaccard: " , jaccard_filtered)
 
-    
-    """
-    s = Shingling("../data/1.txt")
-    s.load_clean_document()
-    s.build_shingles(5)
-    s.hash_shingles()
+    end_time = time.time()
+    print("Time Elapsed Jaccard: ", end_time - start_time)
+    print("\n")
 
-        
-    sh1 = s.hashed_shingles
 
-    s.build_shingles(10)
-    s.hash_shingles()
+    minhashing_objects = []
+    for i, shingling in enumerate(shingling_objects):
+        minhashing_objects.append(MinHashing())
+        minhashing_objects[i].compute_signature(args.n_hash_functions, shingling.hashed_shingles)
 
-    sh2 = s.hashed_shingles
+    pairs_minh = list(itertools.combinations(minhashing_objects, 2))
 
-    print(sh1 == sh2) 
-    
+    start_time_minh = time.time()
 
-    m = MinHashing()
-    print(m.compute_signature(n_hash_functions=100, shingles_list=s.hashed_shingles))
-    """
+    minhashing_list = []
+    for i in range(len(pairs_minh)):
+        minhashing_list.append(c_sign.compare_signatures(pairs_minh[i][0].signature, pairs_minh[i][1].signature))
+        if (args.verbose): print("Signature Similarity between " + str(pairs[i][0].document_id) + " and " + str(pairs[i][1].document_id) + ": " + str(minhashing_list[i]))
+
+    minhashing_filtered = []
+    for i in range(len(minhashing_list)):
+        if minhashing_list[i] >= args.similarity_threshold:
+            minhashing_filtered.append(tuple((pairs[i][0].document_id, pairs[i][1].document_id)))
+
+    print("Filtered couples over Threshold - MinHashing: " , minhashing_filtered)
+
+    end_time_minh = time.time()
+    print("Time Elapsed MinHashing: ", end_time_minh - start_time_minh)
+    print("\n")
+
+    signature_dict = {}
+
+    for i, minhashing in enumerate(minhashing_objects):
+        signature_dict[i + 1] = minhashing.signature
+
+    start_time_lsh = time.time()
+
+    lsh = LSH(signature_dict)
+    lsh.find_pairs(args.bands, args.similarity_threshold)
+
+    print("Filtered couples over Threshold - LSH: ", lsh.final_pairs)
+
+    end_time_lsh = time.time()
+    print("Time Elapsed LSH: ", end_time_lsh - start_time_lsh)
+
+
+
+
