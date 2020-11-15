99d73c0c37ca07b92573259fd6bd6b9cf79dec16
edoardorealini <realini.edoardo@gmail.com>
2020-11-15 14:19:12 +0100

    Association rules

---
+++ hw_2/src/AssociationRules.py
@@ -0,0 +1,17 @@
+def generate_association_rules(frequent_itemset):
+    # Given one single itemset as input, the function generates all the possible association rules in form of tuples
+    # (antecedent, consequence)
+
+    return
+
+
+def filter_association_rules(association_rules_list, confidence_threshold):
+    # Main idea here, if I find one association rule that is below confidence I can skip all the others that have less literals in the antecedent.
+
+    return
+
+
+def compute_confidence(association_rule_tuple):
+    # Compute the confidence level of the given association rule
+
+    return
