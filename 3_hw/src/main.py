b842f5a22355e9a7db367527da1c251a63b16798
Adriano Mundo <adrianomundo23@gmail.com>
2020-11-23 22:19:01 +0100

    final commit hw3

--- 3_hw/src/main.py
+++ 3_hw/src/main.py
@@ -1,21 +1,56 @@
+import argparse
+import time
+
 from graph_reader import GraphReader
 from graph.Graph import Graph
-
 from CentralityHyperBall import  CentralityHyperBall
 
-airport_data = '../data/airport_graph.txt'
+if __name__ == '__main__':
 
-reader = GraphReader(path=airport_data,is_undirected=True)
-graph = reader.read_graph()
+	print("HyperBall algorithm for Mining Data Streams")
+	print("Adriano Mundo & Edoardo Realini")
+	print("KTH Royal Institute of Technology - 2020\n")
 
-hyperball = CentralityHyperBall(graph=graph, num_buckets=32)
-hyperball.compute_hyper_balls()
+	parser = argparse.ArgumentParser(description='Parameters')
+	parser.add_argument('-n', dest='n_nodes', type=int, help='Number of nodes to show', nargs='?', default=10)
+	parser.add_argument('-b', dest='n_buckets', type=int, help='Number of buckets', nargs='?', default=32)
 
-ids = [el for el in range(10)]
+	args = parser.parse_args()
 
-for idx in ids:
-    node = graph.get_node(id=idx)
-    print("Node {} Closeness: ".format(idx), hyperball.calculate_closeness(node))
-    print("Node {} Lin: ".format(idx), hyperball.calculate_lin(node))
-    print("Node {} Harmonic: ".format(idx), hyperball.calculate_harmonic(node))
+	print("Number of nodes: ", args.n_nodes)
+	n_nodes = args.n_nodes
+	n_buckets = args.n_buckets
+
+	filename = '../data/airport_graph.txt'
+
+	reader = GraphReader(path=filename, is_undirected=True)
+	graph = reader.read_graph()
+
+	start = time.time()
+	print("\nStart the HyperBall algorithm\n")
+
+	hyperball = CentralityHyperBall(graph=graph, num_buckets=n_buckets)
+	hyperball.compute_hyper_balls()
+
+	ids = [el for el in range(n_nodes)]
+
+	for idx in ids:
+		node = graph.get_node(id=idx)
+		print("Node {} Closeness: ".format(idx), hyperball.calculate_closeness(node))
+		print("Node {} Lin: ".format(idx), hyperball.calculate_lin(node))
+		print("Node {} Harmonic: ".format(idx), hyperball.calculate_harmonic(node))
+
+	end = time.time()
+	print("\nEnd the HyperBall algorithm, time elapsed: ", end - start, " sec.")
+
+
+
+
+
+
+
+
+
+
+
 
